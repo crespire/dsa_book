eefb20b8126e3e21deb82e0cbf0444bf386dd716
crespire <li.simmon@gmail.com>
2022-12-23 10:32:04 -0500

    feat: add solution for ch8 ex1

--- lib/ch8.rb
+++ lib/ch8.rb
@@ -1,8 +1,20 @@
+##
+# #1
+# Write a function that returns the intersection of two arrays.
+def array_intersection(arr1, arr2)
+  intersection = []
+  seen = Hash.new(false)
+  arr1.each { |value| seen[value] = true }
+  arr2.each { |value| intersection << value if seen[value] }
+  intersection
+end
+
+##
+# #4
+# Write a function that returns the first non-duplicated character in a string.
 def first_non_repeating_letter(input)
   seen = Hash.new(nil)
-  input.downcase.chars.each_with_index do |char, i|
-    seen[char] = seen[char].nil? ? i : -1
-  end
+  input.downcase.chars.each_with_index { |char, i| seen[char] = seen[char].nil? ? i : -1 }
 
   res = nil
   seen.each_value do |v|
--- spec/ch8_spec.rb
+++ spec/ch8_spec.rb
@@ -1,11 +1,21 @@
 require 'ch8'
 
 RSpec.describe 'chapter 8' do
-  it 'passes basic tests' do
-    expect(first_non_repeating_letter('a')).to eq('a')
-    expect(first_non_repeating_letter('aaaaa')).to eq('')
-    expect(first_non_repeating_letter('minimum')).to eq('n')
-    expect(first_non_repeating_letter('sTresS')).to eq('T')
-    expect(first_non_repeating_letter('MoOnmeN')).to eq('e')
+  context 'exercise #1' do
+    it 'passes basic tests' do
+      expect(array_intersection([1, 2, 3, 4, 5], [0, 2, 4, 6, 8])).to eq([2, 4])
+      expect(array_intersection(%w[green blue red yellow], %w[yellow purple pink])).to eq(['yellow'])
+      expect(array_intersection([6, 0, 12, 10, 16], [3, 15, 18, 20, 15])).to eq([])
+    end
+  end
+
+  context 'exercise #4' do
+    it 'passes basic tests' do
+      expect(first_non_repeating_letter('a')).to eq('a')
+      expect(first_non_repeating_letter('aaaaa')).to eq('')
+      expect(first_non_repeating_letter('minimum')).to eq('n')
+      expect(first_non_repeating_letter('sTresS')).to eq('T')
+      expect(first_non_repeating_letter('MoOnmeN')).to eq('e')
+    end
   end
 end
